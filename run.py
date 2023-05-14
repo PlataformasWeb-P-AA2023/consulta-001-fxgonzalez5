62ff27287d4e5e4a501e8c69b5809b8090716365
Francisco González <fxgonzalez5@utpl.edu.ec>
2023-05-14 01:58:59 -0500

    consulta específica

--- run.py
+++ run.py
@@ -15,8 +15,17 @@ if collection.count_documents({}) == 0:
     collection.insert_many(data)
 else:
     # Consulta de todos los registros ingresados a la base de datos
+    print("Todas los registros:")
+    print("Cantidad de registros", collection.count_documents({}))
     docs = collection.find()
     for doc in docs:
+        print(doc)
+
+    # Consulta especifica en la base de datos de los registros
+    print("Registros de la fecha 2015-01-05")
+    print("Cantidad de registros:", collection.count_documents({"Date": "2015-01-05"}))
+    docs1 = collection.find({"Date": "2015-01-05"})
+    for doc in docs1:
         print(doc)
 
 connection.close()
